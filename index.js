61f0598db1e5552134a2b3338fe7dea7410d7aa1
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-28 20:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(obj, cb) {
+    const arr = []
+    for (const key in obj) {
+      arr.push(cb(obj[key]))
+    }
+    return arr
+  }
+
+  function reduce(obj, cb, accumulator) {
+    const keys = Object.keys(obj)
+    let i = 0
+
+    if (accumulator) {
+      i = 1
+    } else {
+      accumulator = obj[keys[1]]
+    }
+
+    for (i; i < keys.length; i++) {
+      accumulator = cb(accumulator, obj[keys[i]])
+    }
+    return accumulator
+  } 
